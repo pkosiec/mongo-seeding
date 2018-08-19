b5d0f2eaa0ab95163412f7dbcad4587bc480933e
Paweł Kosiec <pawel.kosiec@gmail.com>
2018-08-19 18:14:22 +0200

    Improve CI setup (#17)
    * Rename `tests` directory to `test`
    * Reorganize CI scripts

---
+++ .ci/run_in_all_projects.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+TASK_TYPE=$1
+CI_DIR_NAME=".ci"
+
+echo "=== Task $TASK_TYPE ==="
+
+echo "Setting environmental variables..."
+export CI_BUILD_NUMBER=$TRAVIS_BUILD_NUMBER
+export CI_PULL_REQUEST=$TRAVIS_PULL_REQUEST
+export CI_PULL_REQUEST_BRANCH=$TRAVIS_PULL_REQUEST_BRANCH
+export CI_BRANCH=$TRAVIS_BRANCH
+
+echo "Scanning all directories on root..."
+for dir in */ ; do
+    SCRIPT_PATH="$dir$CI_DIR_NAME/$TASK_TYPE.sh"
+    if [ ! -f "$SCRIPT_PATH" ]; then
+        continue
+    fi
+    
+    echo ">> Running $TASK_TYPE in directory $dir..."
+    source $SCRIPT_PATH
+done
---
+++ docker-image/.ci/after_success.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "Tagging Docker image..."
+cd ./docker-image
+
+NPM_VERSION=$(node -p -e "require('./package.json').version");
+echo "Package version: $NPM_VERSION";
+
+TAG=$(if [[ $CI_PULL_REQUEST == "false" ]] && [[ $CI_BRANCH == "master" ]]; then
+  echo "latest";
+  elif [[ $CI_PULL_REQUEST == "false" ]]; then
+  echo "snapshot-branch-$CI_BRANCH";
+  else
+  echo "snapshot-PR-$CI_PULL_REQUEST_BRANCH";
+fi)
+
+if [[ $CI_PULL_REQUEST == "false" ]] && [[ $CI_BRANCH == "master" ]]; then
+  ALREADY_EXISTS=$(curl https://hub.docker.com/v2/repositories/$DOCKER_IMAGE_REPOSITORY/tags/?page_size=10000 | jq -r "[.results | .[] | .name == \"$NPM_VERSION\"] | any")
+fi
+
+if [[ $ALREADY_EXISTS == "true" ]]; then
+  echo "Skipping pushing image with the same NPM Version tag";
+  exit 0;
+fi
+
+echo "Tagging the Docker image with $TAG tag...";
+docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
+docker tag $DOCKER_IMAGE_REPOSITORY:$CI_BUILD_NUMBER $DOCKER_IMAGE_REPOSITORY:$TAG;
+
+echo "Pushing the Docker image with $TAG tag..."
+docker push $DOCKER_IMAGE_REPOSITORY:$TAG;
+
+# Push additional tag with version if necessary
+if [[ $CI_PULL_REQUEST == "false" ]] && [[ $CI_BRANCH == "master" ]]; then
+  echo "Tagging the Docker image with $NPM_VERSION tag...";
+  docker tag $DOCKER_IMAGE_REPOSITORY:$CI_BUILD_NUMBER $DOCKER_IMAGE_REPOSITORY:$NPM_VERSION;
+  echo "Pushing the Docker image with $NPM_VERSION tag...";
+  docker push $DOCKER_IMAGE_REPOSITORY:$NPM_VERSION;
+fi
