40ba5f5eff92052d19af82443e23e7f5311ab9e9
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-09-15 15:51:41 +0200

    Include ObjectId helpers in Core (#88)

---
+++ core/src/helpers/index.ts
@@ -0,0 +1,30 @@
+import { ObjectId } from 'mongodb';
+import { createHash } from 'crypto';
+
+/**
+ * Constructs an ObjectId instance based on entity name.
+ * Helpful for database references between two collections.
+ *
+ * @param name Entity name
+ */
+export const getObjectId = (name: string): ObjectId => {
+  if (name === '') {
+    throw new Error('Name cannot be empty');
+  }
+
+  const hash = createHash('sha1')
+    .update(name, 'utf8')
+    .digest('hex');
+
+  return new ObjectId(hash.substring(0, 24));
+};
+
+/**
+ * Constructs ObjectId instances based on entity names.
+ * Helpful for database references between two collections.
+ *
+ * @param names Array of object names
+ */
+export const getObjectIds = (names: string[]): ObjectId[] => {
+  return names.map(name => getObjectId(name));
+};
