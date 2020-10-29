20da381f89928c185a52eefe776681e67ec59118
Pawel Kosiec <pawel.kosiec@gmail.com>
2020-10-29 22:18:08 +0100

    Fix reporting coverage for PR from forks

--- core/.ci/script.sh
+++ core/.ci/script.sh
@@ -10,12 +10,5 @@ echo "Building test image..."
 docker build -t $IMAGE_NAME:$CI_BUILD_NUMBER -f ./core/Dockerfile .
 
 echo "Running tests with coverage reporting..."
-
-RUN_COMMAND=""
-if [ "${CI_SECURE_ENV_VARS}" == "false" ]; then
-    echo "No secret environmental variables - coverage reporting disabled."
-    RUN_COMMAND="npm test"
-fi
-
 CI_ENV=`bash <(curl -s https://codecov.io/env)`
-docker run --rm --network="host" $CI_ENV -e CODECOV_TOKEN=$CODECOV_TOKEN $IMAGE_NAME:$CI_BUILD_NUMBER $RUN_COMMAND
+docker run --rm --network="host" $CI_ENV -e CODECOV_TOKEN=$CODECOV_TOKEN $IMAGE_NAME:$CI_BUILD_NUMBER
