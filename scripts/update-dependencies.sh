1939d361519a91f779337f55f491cd0e00a7c3ba
Paweł Kosiec <pawel.kosiec@gmail.com>
2021-06-12 15:34:29 +0200

    Remove dev dependencies for git pre-commit hooks

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# standard bash error handling
+set -o nounset # treat unset variables as an error and exit immediately.
+set -o errexit # exit immediately when a command fails.
+set -E         # needs to be set if we want the ERR trap
+
 ROOT_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )
 
 DIRECTORIES=(
