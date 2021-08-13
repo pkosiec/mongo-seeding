8719cadb7d2f482c7525ef6b7328fa75945dfa02
Timo Rohrberg <tools@softwarexpert24.de>
2021-08-13 10:55:49 +0200

    Fix #180 - Added additional prod Dockerfile building image based on regular node image.

--- scripts/publish-npm-and-docker.sh
+++ scripts/publish-npm-and-docker.sh
@@ -6,6 +6,7 @@ set -o errexit # exit immediately when a command fails.
 set -E         # needs to be set if we want the ERR trap
 
 IMAGE_NAME="pkosiec/mongo-seeding"
+IMAGE_NAME_NODE="pkosiec/mongo-seeding:node"
 IMAGE_LATEST_TAG="latest"
 
 set -e
@@ -22,6 +23,7 @@ IMAGE_VERSION_TAG=$(node -p -e "require('./cli/package.json').version");
 echo "Using tag: ${IMAGE_VERSION_TAG}"
 
 docker build -f ./docker-image/prod.Dockerfile --build-arg cliVersion=${IMAGE_VERSION_TAG} -t ${IMAGE_NAME}:${IMAGE_VERSION_TAG} .
+docker build -f ./docker-image/prod.Dockerfile.node --build-arg cliVersion=${IMAGE_VERSION_TAG} -t ${IMAGE_NAME_NODE} .
 
 echo "Tagging Docker image with '${IMAGE_LATEST_TAG}' tag..."
 docker tag ${IMAGE_NAME}:${IMAGE_VERSION_TAG} ${IMAGE_NAME}:${IMAGE_LATEST_TAG}
@@ -29,3 +31,4 @@ docker tag ${IMAGE_NAME}:${IMAGE_VERSION_TAG} ${IMAGE_NAME}:${IMAGE_LATEST_TAG}
 echo "Pushing images..."
 docker push ${IMAGE_NAME}:${IMAGE_VERSION_TAG}
 docker push ${IMAGE_NAME}:${IMAGE_LATEST_TAG}
+docker push ${IMAGE_NAME_NODE}
