e5f0667e5ab8e7ebf6d4e3561ed3423720e3a71b
Pawel Kosiec <pawel.kosiec@gmail.com>
2023-10-02 21:50:58 +0200

    Update dependencies

--- core/src/index.ts
+++ core/src/index.ts
@@ -121,8 +121,9 @@ export class Seeder {
 
       if (!config.dropDatabase && config.removeAllDocuments) {
         this.log('Removing all documents from collections...');
-        const promises = collections.map((collection) =>
-          database?.removeAllDocumentsIfCollectionExists(collection.name),
+        const promises = collections.map(
+          (collection) =>
+            database?.removeAllDocumentsIfCollectionExists(collection.name),
         );
         await Promise.all(promises);
       }
